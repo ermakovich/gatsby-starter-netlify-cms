338499fd47bfd4a2a4f3d1a292f4326afc9a3cd9
Sergey Ermakovich <s.ermakovich@gmail.com>
2018-06-03 20:07:34 +0300

    cleanup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,17 +9,12 @@ const Navbar = () => (
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+          <h1>МОЯ территория</h1>
         </Link>
       </div>
       <div className="navbar-start">
         <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
+          Обо Мне
         </Link>
       </div>
       <div className="navbar-end">
